b900abf9e23944869b6d7e5ea6b6af5b6067feda
corvus meliora <corvus@zestic.com>
2025-09-20 15:11:23 -0500

    remove layout, its not being used now

--- src/layout/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview Layout domain exports
- *
- * All layout-related functionality including managers and interfaces.
- */
-
-// Re-export everything from this domain
-export * from './types';
-export { LayoutManager } from './LayoutManager';
